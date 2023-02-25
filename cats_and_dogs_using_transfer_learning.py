c43f4bfe2ab24b63e87e6fb6a807a039e99a2ef9
Gerald <geraldwainaina25@gmail.com>
2023-02-25 17:27:34 +0300

    Update cats_and_dogs_using_transfer_learning.py

--- cats_and_dogs_using_transfer_learning.py
+++ cats_and_dogs_using_transfer_learning.py
@@ -10,3 +10,19 @@ logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
 
 #Download classifier
+CLASSIFIER_URL ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
+IMAGE_RES = 224
+
+model = tf.keras.Sequential([
+    hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+])
+
+#single image
+import numpy as np
+import PIL.Image as Image
+
+grace_hopper = tf.keras.utils.get_file('image.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/grace_hopper.jpg')
+grace_hopper = Image.open(grace_hopper).resize((IMAGE_RES, IMAGE_RES))
+grace_hopper
+grace_hopper = np.array(grace_hopper)/255.0
+grace_hopper.shape

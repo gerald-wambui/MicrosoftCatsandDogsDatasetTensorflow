98be250183de7a88f1c0642e80a56b27c163ba7b
Gerald <geraldwainaina25@gmail.com>
2023-02-25 08:00:27 +0300

    cool

--- cats_and_dogs_using_transfer_learning.py
+++ cats_and_dogs_using_transfer_learning.py
@@ -2,3 +2,11 @@
 import tensorflow as tf
 import matplotlib.pylab as plt
 import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+#Download classifier
